a40e80183ab587706cdf932da5664d85b9229e16
Blessingngo <blessingvictor998@gmail.com>
2023-06-02 11:59:23 +0100

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,14 +6,10 @@ section .text
 
 	global main
 	main:
-		sub rsp, 8     ; Align stack on 16-byte boundary (required for macOS)
-
-	mov edi, message
-	xor eax, eax   ; Set format specifier to "%s"
-	call printf
-
-	add rsp, 8     ; Restore stack alignment
-	mov eax, 0x60  ; Exit syscall number
-	xor edi, edi   ; Exit status (0)
-	syscall
+		mov edi, message
+		xor eax, eax   ; Set format specifier to "%s"
+		call printf
+		mov eax, 0
+		ret
+	format: db 'Hello, Holberton\n',0
 
