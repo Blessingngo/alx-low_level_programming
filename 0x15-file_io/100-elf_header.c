ec7177c95cec00aeeadfa47faa73f751d67c3836
Blessing Victor <blessingvictor998@gmail.com>
2023-09-04 08:40:05 -0700

    File update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,28 +18,6 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - function that checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- * Description: If the file is not an ELF file - exit code 98.
- */
-void check_elf(unsigned char *e_ident)
-{
-	int index;
-
-	for (index = 0; index < 4; index++)
-{
-	if (e_ident[index] != 127 &&
-	e_ident[index] != 'E' &&
-	e_ident[index] != 'L' &&
-	e_ident[index] != 'F')
-{
-	dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-	exit(98);
-}
-}
-}
-
-/**
  * print_magic - function that prints the magic numbers of an ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  * Description: Magic numbers are separated by spaces.
@@ -238,21 +216,6 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 	else
 	printf("%#lx\n", e_entry);
-}
-
-/**
- * close_elf - function that closes an ELF file.
- * @elf: The file descriptor of the ELF file.
- * Description: If the file cannot be closed - exit code 98.
- */
-void close_elf(int elf)
-{
-	if (close(elf) == -1)
-{
-	dprintf(STDERR_FILENO,
-	"Error: Can't close fd %d\n", elf);
-	exit(98);
-}
 }
 
 /**
